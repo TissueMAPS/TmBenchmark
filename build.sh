96e56ac9e4c73ee79125e80eb77ad38c42148741
hackermd <markusdherrmann@gmail.com>
2017-06-24 16:19:18 +0000

    Add script for provisioning and deployment

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+set -e
+
+NAME=$1
+
+if [[ -z $NAME ]]
+then
+    echo "Error: Name of the architecture must be provided."
+    exit 1
+fi
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+
+BASE_COMMAND () {
+    cmd=$1
+    /usr/bin/time --verbose tm_deploy -v vm -s "$SCRIPT_DIR/setup/$NAME.yaml" "$cmd" > "$SCRIPT_DIR/log/$NAME.$cmd.log" 2>&1;
+}
+
+BASE_COMMAND launch
+
+BASE_COMMAND deploy
+
+exit 0
